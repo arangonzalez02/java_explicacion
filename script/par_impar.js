038bc203c9ac1d37f50de6cb88b12de29f0df469
arangonzalez02 <aran.gonzalez.garcia02@gmail.com>
2024-05-17 09:46:02 +0200

    cambios

---
+++ script/par_impar.js
@@ -0,0 +1,20 @@
+let boton=document.querySelector(".boton");
+
+boton.addEventListener("click", function (){
+    let num=document.querySelector("#numero").ariaValueMax;
+    if(num==""){
+        document.querySelector(".mensajeError").innerHTML="Debe ingresar un número para poderle indicar si es par o impar."
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+})
